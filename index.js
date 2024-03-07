181f0e1045f3e862dcb1b88a3f9261e2a9490cba
tiger <44923556+eeeeaa@users.noreply.github.com>
2024-03-07 19:44:35 +0700

    add 404 error

--- index.js
+++ index.js
@@ -19,8 +19,13 @@ app.get("/contact-me", (req, res) => {
   res.sendFile(path.join(__dirname + "/routes/contact-me.html"));
 });
 
-app.use(function (req, res, next) {
-  res.sendFile(path.join(__dirname + "/routes/404.html"));
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname + "/routes/404.html"));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 app.listen(port, () => {
